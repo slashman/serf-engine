b3d0b56ecd2c2e2f4ddb10352a7b9abb6ab50c72
java.koder <java.koder@51ccf7a8-1371-11de-a6cc-771c510851eb>
2009-03-19 01:17:36 +0000

    Abstract Level no longer stores mapcell data, responsibility delegated to BufferedLevel.
    This level implementation stores the complete mapcell data in memory.

---
+++ trunk/src/net/slashie/serf/level/BufferedLevel.java
@@ -0,0 +1,87 @@
+package net.slashie.serf.level;
+
+import net.slashie.utils.Position;
+
+public class BufferedLevel extends AbstractLevel{
+	private AbstractCell[][][] map;
+	private boolean[][][] visible;
+	private boolean[][][] lit;
+	private boolean[][][] remembered;
+	
+	public AbstractCell getMapCell(int x, int y, int z){
+		if (z<map.length && x<map[0].length && y < map[0][0].length && x >= 0 && y >= 0 && z >= 0)
+			return map[z][x][y];
+		else return null;
+	}
+	
+	public void setCells(AbstractCell[][][] what){
+		map = what;
+		visible= new boolean[what.length][what[0].length][what[0][0].length];
+		lit= new boolean[what.length][what[0].length][what[0][0].length];
+		remembered= new boolean[what.length][what[0].length][what[0][0].length];
+	}
+	
+	@Override
+	public int getWidth(){
+		return map[0].length;
+	}
+
+	@Override
+	public int getHeight(){
+		return map[0][0].length;
+
+	}
+	
+	@Override
+	public int getDepth(){
+		return map.length;
+	}
+	
+	@Override
+	protected void markLit(int x, int y, int z) {
+		lit[z][x][y] = true;
+	}
+
+	public AbstractCell[][][] getCells(){
+		return map;
+	}
+	
+	@Override
+	public void darken(int x, int y, int z){
+		if (!isValidCoordinate(x,y,z))
+			return;
+		visible[z][x][y]= false;
+	}
+
+	@Override
+	public boolean remembers(int x, int y, int z){
+		if (!isValidCoordinate(x,y,z))
+			return false;
+		return remembered[z][x][y];
+	}
+	
+	@Override
+	public boolean isVisible(int x, int y, int z){
+		if (!isValidCoordinate(x,y,z))
+			return false;
+		return visible[z][x][y];
+	}
+	
+	@Override
+	protected boolean isLit(Position p) {
+		return lit[p.z][p.x][p.y];
+	}
+	
+	@Override
+	protected void markRemembered(int x, int y, int z) {
+		remembered[z][x][y] = true;
+	}
+	
+	@Override
+	protected void markVisible(int x, int y, int z) {
+		visible[z][x][y] = true;
+	}	
+	
+	
+
+}
