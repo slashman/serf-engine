a8ce8de39b931bb240ab8e1c72e934881dd52aed
java.koder <java.koder@51ccf7a8-1371-11de-a6cc-771c510851eb>
2009-03-22 07:03:11 +0000

    Adjustements. It works now (Tested on expedition)

--- trunk/src/net/slashie/serf/level/GridLevelReader.java
+++ trunk/src/net/slashie/serf/level/GridLevelReader.java
@@ -1,10 +1,10 @@
 package net.slashie.serf.level;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 
 import net.slashie.serf.action.Actor;
@@ -13,31 +13,60 @@ import net.slashie.serf.baseDomain.AbstractItem;
 import net.slashie.serf.baseDomain.AbstractItemFactory;
 import net.slashie.serf.game.SworeGame;
 import net.slashie.util.Pair;
-import net.slashie.utils.FileUtil;
 import net.slashie.utils.Position;
 import net.slashie.utils.Util;
 
 public class GridLevelReader extends AbstractLevel {
+	private static final int MAX_BUFFERS = 10;
 	private int levelHeight, levelWidth;
 	private int gridHeight, gridWidth;
+
 	private String levelNameset;
 	private Hashtable<String, String> charMap;
 	
-	public GridLevelReader(String levelNameset, int levelWidth, int levelHeight, int gridWidth, int gridHeight, Hashtable<String, String> charmap, Pair<String, Position> mainExit){
+	public GridLevelReader(String levelNameset, int levelWidth, int levelHeight, int gridWidth, int gridHeight, Hashtable<String, String> charmap, Position startPosition){
 		this.levelNameset = levelNameset;
 		this.levelWidth = levelWidth;
 		this.levelHeight = levelHeight;
 		this.gridWidth = gridWidth;
 		this.gridHeight = gridHeight;
 		this.charMap = charmap;
-		addExit(mainExit.getB(), mainExit.getA());
+		//addExit(mainExit.getB(), mainExit.getA());
+		getMapCell(startPosition);
 	}
 
 	private Map<String,AbstractCell[][]> buffers = new Hashtable<String, AbstractCell[][]>();
+	private Map<String,boolean[][]> visible = new Hashtable<String, boolean[][]>();
+	private Map<String,boolean[][]> lit = new Hashtable<String, boolean[][]>();
+	private Map<String,boolean[][]> remembered = new Hashtable<String, boolean[][]>();
+	
+	private List<String> bufferIds = new ArrayList<String>();
+	
+	@Override
+	public void darken() {
+		for (String bufferId: bufferIds){
+			boolean[][] visibleX = visible.get(bufferId);
+			for (int x = 0; x < visibleX.length; x++)
+				for (int y = 0; y < visibleX[0].length; y++)
+					visibleX[x][y] = false;
+		}
+	}
 	
 	@Override
 	protected void darken(int x, int y, int z) {
-		// TODO Auto-generated method stub
+		if (!isValidCoordinate(x, y, z))
+			return;
+		
+		//Calculate the bigX and bigY for the requestedCell
+		int bigX = (int)Math.floor((double)x/(double)gridWidth);
+		int bigY = (int)Math.floor((double)y/(double)gridHeight);
+		String bufferId = bigX+"-"+bigY;
+		boolean[][] buffer = visible.get(bufferId);
+		if (buffer == null){
+			readBuffer(bigX, bigY);
+			buffer = visible.get(bufferId);
+		}
+		buffer[x%gridWidth][y%gridHeight] = false;		
 	}
 
 	@Override
@@ -52,6 +81,9 @@ public class GridLevelReader extends AbstractLevel {
 
 	@Override
 	public AbstractCell getMapCell(int x, int y, int z) {
+		if (!isValidCoordinate(x, y))
+			return null;
+		
 		//Calculate the bigX and bigY for the requestedCell
 		int bigX = (int)Math.floor((double)x/(double)gridWidth);
 		int bigY = (int)Math.floor((double)y/(double)gridHeight);
@@ -61,27 +93,39 @@ public class GridLevelReader extends AbstractLevel {
 			//Read Buffer
 			buffer = readLevelBuffer(levelNameset, bigX, bigY);
 			buffers.put(bufferId, buffer);
+			visible.put(bufferId, new boolean[gridWidth][gridHeight]);
+			lit.put(bufferId, new boolean[gridWidth][gridHeight]);
+			remembered.put(bufferId, new boolean[gridWidth][gridHeight]);
+			
+			bufferIds.add(bufferId);
+			if (bufferIds.size() > MAX_BUFFERS){
+				removeOldBuffer();
+			}
 		}
-		return buffer[x%gridWidth][x%gridHeight];
+		return buffer[x%gridWidth][y%gridHeight];
+	}
+	
+	private void removeOldBuffer(){
+		String oldestBufferId = bufferIds.get(0);
+		bufferIds.remove(0);
+		buffers.remove(oldestBufferId);
+		visible.remove(oldestBufferId);
+		lit.remove(oldestBufferId);
+		remembered.remove(oldestBufferId);
 	}
 	
 	private AbstractCell[][] readLevelBuffer(String levelNameset, int bigX, int bigY){
 		try {
-			//String filename = "maps/"+levelNameset+"-"+bigX+"-"+bigY+".fragment";
+			int charMultiplier = 1;
 			RandomAccessFile raf = new RandomAccessFile(levelNameset+".txt", "r");
 			String[] map = new String[gridHeight];
-			raf.seek(2*bigY*(levelWidth+1) + 2*bigX*gridWidth*gridHeight);
-			byte[] buffer = new byte[gridWidth*2];
+			//raf.seek(2*bigY*(levelWidth+1) + 2*bigX*gridWidth*gridHeight);
+			long gridLength = gridWidth * gridHeight;
+			int bigcellsWidth = (int)Math.ceil((double)levelWidth/(double)gridWidth);
+			raf.seek(charMultiplier * (bigY*gridLength*bigcellsWidth+ bigX*gridLength));
+			byte[] buffer = new byte[gridWidth*charMultiplier];
 			int bytes = raf.read(buffer);
-            /*String line = raf.readLine();
-            int yr = 0;
-            while (line != null){
-            	map[yr]=line;
-            	line = raf.readLine();
-            	yr++;
-            }
-            raf.close();*/
-			
+            
 			int yr = 0;
             while (bytes > -1 && yr < gridHeight){
             	map[yr]= new String(buffer);
@@ -90,7 +134,7 @@ public class GridLevelReader extends AbstractLevel {
             }
             
             AbstractCell [][] ret = new AbstractCell[map[0].length()][map.length];
-    	    Position where = new Position(0,0,0);
+    	    Position where = new Position(bigX*gridWidth,bigY*gridHeight,0);
     	    
 	    	for (int y = 0; y < map.length; y++){
 				for (int x = 0; x < map[0].length(); x++) {
@@ -145,40 +189,122 @@ public class GridLevelReader extends AbstractLevel {
 		return levelWidth;
 	}
 
+	private void readBuffer(int bigX, int bigY){
+		String bufferId = bigX+"-"+bigY;
+		//Read Buffer
+		AbstractCell[][] buffer = readLevelBuffer(levelNameset, bigX, bigY);
+		buffers.put(bufferId, buffer);
+		visible.put(bufferId, new boolean[gridWidth][gridHeight]);
+		lit.put(bufferId, new boolean[gridWidth][gridHeight]);
+		remembered.put(bufferId, new boolean[gridWidth][gridHeight]);
+		
+		bufferIds.add(bufferId);
+		if (bufferIds.size() > MAX_BUFFERS){
+			removeOldBuffer();
+		}
+	}
 	@Override
 	protected boolean isLit(Position p) {
-		// TODO Auto-generated method stub
-		return true;
+		int x = p.x;
+		int y = p.y;
+		if (!isValidCoordinate(p.x, p.y))
+			return false;
+		
+		//Calculate the bigX and bigY for the requestedCell
+		int bigX = (int)Math.floor((double)x/(double)gridWidth);
+		int bigY = (int)Math.floor((double)y/(double)gridHeight);
+		String bufferId = bigX+"-"+bigY;
+		boolean[][] buffer = lit.get(bufferId);
+		if (buffer == null){
+			readBuffer(bigX, bigY);
+			buffer = lit.get(bufferId);
+		}
+		return buffer[x%gridWidth][y%gridHeight];
 	}
 
 	@Override
 	public boolean isVisible(int x, int y, int z) {
-		// TODO Auto-generated method stub
-		return true;
+		if (!isValidCoordinate(x, y, z))
+			return false;
+		
+		//Calculate the bigX and bigY for the requestedCell
+		int bigX = (int)Math.floor((double)x/(double)gridWidth);
+		int bigY = (int)Math.floor((double)y/(double)gridHeight);
+		String bufferId = bigX+"-"+bigY;
+		boolean[][] buffer = visible.get(bufferId);
+		if (buffer == null){
+			readBuffer(bigX, bigY);
+			buffer = visible.get(bufferId);
+		}
+		return buffer[x%gridWidth][y%gridHeight];
 	}
 
 	@Override
 	protected void markLit(int x, int y, int z) {
-		// TODO Auto-generated method stub
+		if (!isValidCoordinate(x, y, z))
+			return;
 		
+		//Calculate the bigX and bigY for the requestedCell
+		int bigX = (int)Math.floor((double)x/(double)gridWidth);
+		int bigY = (int)Math.floor((double)y/(double)gridHeight);
+		String bufferId = bigX+"-"+bigY;
+		boolean[][] buffer = lit.get(bufferId);
+		if (buffer == null){
+			readBuffer(bigX, bigY);
+			buffer = lit.get(bufferId);
+		}
+		buffer[x%gridWidth][y%gridHeight] = true;
 	}
 
 	@Override
 	protected void markRemembered(int x, int y, int z) {
-		// TODO Auto-generated method stub
+		if (!isValidCoordinate(x, y, z))
+			return;
 		
+		//Calculate the bigX and bigY for the requestedCell
+		int bigX = (int)Math.floor((double)x/(double)gridWidth);
+		int bigY = (int)Math.floor((double)y/(double)gridHeight);
+		String bufferId = bigX+"-"+bigY;
+		boolean[][] buffer = remembered.get(bufferId);
+		if (buffer == null){
+			readBuffer(bigX, bigY);
+			buffer = remembered.get(bufferId);
+		}
+		buffer[x%gridWidth][y%gridHeight] = true;
 	}
 
 	@Override
 	protected void markVisible(int x, int y, int z) {
-		// TODO Auto-generated method stub
+		if (!isValidCoordinate(x, y, z))
+			return;
 		
+		//Calculate the bigX and bigY for the requestedCell
+		int bigX = (int)Math.floor((double)x/(double)gridWidth);
+		int bigY = (int)Math.floor((double)y/(double)gridHeight);
+		String bufferId = bigX+"-"+bigY;
+		boolean[][] buffer = visible.get(bufferId);
+		if (buffer == null){
+			readBuffer(bigX, bigY);
+			buffer = visible.get(bufferId);
+		}
+		buffer[x%gridWidth][y%gridHeight] = true;		
 	}
 
 	@Override
 	protected boolean remembers(int x, int y, int z) {
-		// TODO Auto-generated method stub
-		return true;
+		if (!isValidCoordinate(x, y, z))
+			return false;
+		
+		//Calculate the bigX and bigY for the requestedCell
+		int bigX = (int)Math.floor((double)x/(double)gridWidth);
+		int bigY = (int)Math.floor((double)y/(double)gridHeight);
+		String bufferId = bigX+"-"+bigY;
+		boolean[][] buffer = remembered.get(bufferId);
+		if (buffer == null){
+			readBuffer(bigX, bigY);
+			buffer = remembered.get(bufferId);
+		}
+		return buffer[x%gridWidth][y%gridHeight];
 	}
 	
 	/**
