b13194dd7ee9d585af0e730f525ea1afd95b4798
java.koder <java.koder@51ccf7a8-1371-11de-a6cc-771c510851eb>
2010-08-16 05:29:52 +0000

    Allow specifiying action cost

--- src/net/slashie/serf/action/PassAction.java
+++ src/net/slashie/serf/action/PassAction.java
@@ -1,6 +1,19 @@
 package net.slashie.serf.action;
 
 public class PassAction extends Action{
+	private int cost;
+
+	/**
+	 * @deprecated Specify cost instead
+	 */
+	public PassAction() {
+		this(50);
+	}
+	
+	public PassAction(int cost) {
+		this.cost = cost;
+	}
+	
 	@Override
 	public void execute() {
 		//Do Nothing
@@ -9,5 +22,10 @@ public class PassAction extends Action{
 	@Override
 	public String getID() {
 		return "Pass";
+	}
+	
+	@Override
+	public int getCost() {
+		return cost;
 	}
 }
