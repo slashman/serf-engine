da7f86ef1eff5ffff191679b116a2ff290d4c994
java.koder <java.koder@51ccf7a8-1371-11de-a6cc-771c510851eb>
2010-08-24 03:32:11 +0000

    Use ESC to close windows and dialogs

--- src/net/slashie/utils/swing/BorderedMenuBox.java
+++ src/net/slashie/utils/swing/BorderedMenuBox.java
@@ -96,6 +96,7 @@ public class BorderedMenuBox extends AddornedBorderPanel {
 			List shownItems = Util.page(items, pageElements, currentPage);
 			CharKey key = new CharKey(CharKey.NONE);
 			while (key.code != CharKey.SPACE &&
+					key.code != CharKey.ESC &&
 				   key.code != CharKey.UARROW &&
 				   key.code != CharKey.DARROW &&
 				   key.code != CharKey.N8 &&
@@ -104,7 +105,7 @@ public class BorderedMenuBox extends AddornedBorderPanel {
 				   (key.code < CharKey.a || key.code > CharKey.a + pageElements-1)
 				   )
 			   key = si.inkey();
-			if (key.code == CharKey.SPACE)
+			if (key.code == CharKey.SPACE || key.code == CharKey.ESC)
 				return null;
 			if (key.code == CharKey.UARROW || key.code == CharKey.N8)
 				if (currentPage > 0)
@@ -131,6 +132,7 @@ public class BorderedMenuBox extends AddornedBorderPanel {
 			List shownItems = Util.page(items, pageElements, currentPage);
 			CharKey key = new CharKey(CharKey.NONE);
 			while (key.code != CharKey.SPACE &&
+					key.code != CharKey.ESC &&
 				   key.code != CharKey.UARROW &&
 				   key.code != CharKey.DARROW &&
 				   key.code != CharKey.N8 &&
@@ -140,7 +142,7 @@ public class BorderedMenuBox extends AddornedBorderPanel {
 				   !isOneOf(key.code, keys)
 				   )
 			   key = si.inkey();
-			if (key.code == CharKey.SPACE)
+			if (key.code == CharKey.SPACE || key.code == CharKey.ESC)
 				return null;
 			if (key.code == CharKey.UARROW || key.code == CharKey.N8)
 				if (currentPage > 0)
