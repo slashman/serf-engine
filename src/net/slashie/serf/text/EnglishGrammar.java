922fe5a2947eb9dbc90f97b746461afb5574fe51
java.koder <java.koder@51ccf7a8-1371-11de-a6cc-771c510851eb>
2010-10-22 04:36:19 +0000

    String list

--- src/net/slashie/serf/text/EnglishGrammar.java
+++ src/net/slashie/serf/text/EnglishGrammar.java
@@ -1,5 +1,7 @@
 package net.slashie.serf.text;
 
+import java.util.List;
+
 public class EnglishGrammar {
 	public static String a(String fullDescription) {
 		char firstCharacter = fullDescription.toCharArray()[0];
@@ -23,5 +25,21 @@ public class EnglishGrammar {
 			return text.substring(0, text.length()-1)+"ies";
 		}
 		return text+"s";
+	}
+	
+	public static String stringList (List<String> strings){
+		int i = 0;
+		String ret = "";
+		for (String string: strings){
+			ret += string;
+			if (i == strings.size()-2)
+				ret += " and ";
+			else if (i == strings.size()-1)
+				;
+			else if (strings.size()>1)
+				ret += ", ";
+			i++;
+		}
+		return ret;
 	}
 }
