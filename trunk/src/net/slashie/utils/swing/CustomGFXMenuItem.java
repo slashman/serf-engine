9103ea967f16dabababce411947f6ff2270f5a39
java.koder <java.koder@51ccf7a8-1371-11de-a6cc-771c510851eb>
2011-07-28 00:18:55 +0000

    Send index to tooltips

--- trunk/src/net/slashie/utils/swing/CustomGFXMenuItem.java
+++ trunk/src/net/slashie/utils/swing/CustomGFXMenuItem.java
@@ -5,5 +5,5 @@ import net.slashie.serf.ui.oryxUI.SwingSystemInterface;
 public interface CustomGFXMenuItem extends GFXMenuItem{
 	void drawMenuItem(SwingSystemInterface si, int x, int y, int index, boolean highlight);
 	public boolean showTooltip();
-	public void drawTooltip(SwingSystemInterface si, int xpos, int ypos);
+	public void drawTooltip(SwingSystemInterface si, int xpos, int ypos, int index);
 }
