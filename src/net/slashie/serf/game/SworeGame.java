3663f253a89c0f76664a4626acf80fd8e2c8d062
java.koder <java.koder@51ccf7a8-1371-11de-a6cc-771c510851eb>
2011-08-18 03:11:26 +0000

    v0.19 for Expedition v0.4 alpha1

--- src/net/slashie/serf/game/SworeGame.java
+++ src/net/slashie/serf/game/SworeGame.java
@@ -265,7 +265,7 @@ public abstract class SworeGame implements CommandListener, PlayerEventListener,
 	}
 	
 	public static String getVersion(){
-		return "0.18 - r169";
+		return "0.19 - r172";
 	}
 	
 	public void setInterfaces(UserInterface pui, UISelector ps){
