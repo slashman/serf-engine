603bcc04b62ba1f87137d0d83b4883b24554c7da
java.koder <java.koder@51ccf7a8-1371-11de-a6cc-771c510851eb>
2011-11-12 04:27:07 +0000

    Reactive composite image SEEMS LIKE IT FIXES THE WEIRD TRANSPARENCY ISSUE AT EXPENSE OF PERFORMANCE

--- trunk/src/net/slashie/serf/ui/oryxUI/SwingSystemInterface.java
+++ trunk/src/net/slashie/serf/ui/oryxUI/SwingSystemInterface.java
@@ -1102,19 +1102,19 @@ class SwingInterfacePanel extends JPanel{
 	 */
 	public synchronized void paintComponent(Graphics g){
 		if (layerImages != null && compositeGraphics != null){
-			/*super.paintComponent(compositeGraphics);
+			super.paintComponent(compositeGraphics);
 			compositeGraphics.setColor(Color.BLACK);
 			compositeGraphics.fillRect(0,0,getWidth(),getHeight());
 			for (int i = 0; i < layerImages.length; i++){
 				compositeGraphics.drawImage(layerImages[i], 0,0,null);
 			}
-			g.drawImage(compositeImage, 0, 0, null);*/
-			super.paintComponent(g);
+			g.drawImage(compositeImage, 0, 0, null);
+			/*super.paintComponent(g);
 			g.setColor(Color.BLACK);
 			g.fillRect(0,0,getWidth(),getHeight());
 			for (int i = 0; i < layerImages.length; i++){
 				g.drawImage(layerImages[i], 0,0,null);
-			}
+			}*/
 		}
 	}
 }
